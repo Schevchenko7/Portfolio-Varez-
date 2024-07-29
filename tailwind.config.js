c13029bf5fc6512f147a816be386514d5bf22138
Varez Luniansyah Putra <145889840+Schevchenko7@users.noreply.github.com>
2024-07-30 00:02:12 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        third: '#f9fafb',
+        dark: '#0f172a',
+        cyan: '#67e8f9',
+        green: '#004643',
+        bootstrap: '#7952B3',
+        html: '#E34F26',
+        cc: '#1572B6',
+        figma: '#F24E1E',
+        java: '#F7DF1E',
+        yt: '#FF0000',
+        gt: '#181717',
+        ig: '#E4405F',
+        str: '#FC4C02',
+        gm: '#EA4335',
+        vue: '#4FC08D',
+        tlw: '#06B6D4',
+        lara: '#FF2D20',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
